f3d9d730f89614301ef23fdb9d724c17f713f0b5
Roman Pahl <roman.pahl@gmail.com>
2024-05-05 18:10:47 +0200

    relocate pipeline alias functions into separate file

--- tests/testthat/test_pipeflow_pipeline.R
+++ tests/testthat/test_pipeflow_pipeline.R
@@ -4612,31 +4612,3 @@ test_that("private methods work as expected",
         })
     })
 })
-
-
-
-# pipeline helper functions
-
-test_that("a helper function is defined for each member function
-    with the correct body",
-{
-    skip_if(Sys.getenv("R_CODECOV_ENV") == "GITHUB_ACTION")
-
-    pip <- Pipeline$new("pipe")
-    funs2check <- sapply(names(pip), \(x) is.function(pip[[x]])) |>
-        Filter(f = isTRUE) |>
-        names() |>
-        setdiff("initialize")
-
-    for (fun in funs2check) {
-        helper_fun <- paste0("pipe_", fun)
-        expect_true(exists(helper_fun), info = fun)
-
-        f <- get(helper_fun, envir = asNamespace("pipeflow"))
-        expect_equal(
-            toString(body(f)),
-            gettextf("pip$%s, ...", fun),
-            info = fun
-        )
-    }
-})
