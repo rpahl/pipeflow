8e5416e8f2a56907e85c65a9a63833178f825eb0
Roman Pahl <roman.pahl@gmail.com>
2026-04-19 17:58:46 +0200

    feat: setup package doc

---
+++ R/pipeflow-package.R
@@ -0,0 +1,26 @@
+#' @keywords internal
+#' @useDynLib pipeflow, .registration = TRUE
+"_PACKAGE"
+
+
+.env <- new.env(parent = emptyenv())
+
+.step_states <- new.env(parent = emptyenv())
+.step_states[["new"]] <- c(name = "new", color = "#47b8ffff")
+.step_states[["done"]] <- c(name = "done", color = "#87ff6fff")
+.step_states[["outdated"]] <- c(name = "outdated", color = "#ffa040ff")
+.step_states[["failed"]] <- c(name = "failed", color = "#ff4c4cff")
+
+
+.this_package_name <- function()
+{
+    methods::getPackageName()
+}
+
+
+.this_package_path <- function(...)
+{
+    system.file(package = .this_package_name()) |>
+        file.path(...) |>
+        normalizePath(winslash = "/", mustWork = FALSE)
+}
