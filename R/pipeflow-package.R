df8477b3f69acc10fa83d6df7e2f08c834408695
Roman Pahl <roman.pahl@gmail.com>
2026-08-01 13:08:47 +0200

    docs: remove old docs and further unused helpers

--- R/pipeflow-package.R
+++ R/pipeflow-package.R
@@ -11,15 +11,3 @@
 .step_states[["done"]] <- c(name = "done", color = "#87ff6fff")
 .step_states[["outdated"]] <- c(name = "outdated", color = "#ffa040ff")
 .step_states[["failed"]] <- c(name = "failed", color = "#ff4c4cff")
-
-
-.this_package_name <- function() {
-    methods::getPackageName()
-}
-
-
-.this_package_path <- function(...) {
-    system.file(package = .this_package_name()) |>
-        file.path(...) |>
-        normalizePath(winslash = "/", mustWork = FALSE)
-}
--- tests/testthat/helper_test_data.R
+++
@@ -1,3 +0,0 @@
-testdata_dir <- function() {
-    system.file("testdata", package = methods::getPackageName())
-}
